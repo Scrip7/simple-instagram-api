85d8c6334677688d414e1a3f630c61abbec2c49a
Hooman <37535505+Scrip7@users.noreply.github.com>
2021-03-23 00:24:42 +0430

    lint and added more tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,27 +1,25 @@
-import { InstagramApi } from "./../src/index";
-const URL = require("url").URL;
+import { URL } from 'url';
+import InstagramApi from '../src/index';
 
-const stringIsAValidUrl = (s: string) => {
+const stringIsAValidUrl = (s: string): boolean => {
   try {
-    new URL(s);
-    return true;
+    const valid = new URL(s);
+    return !!valid;
   } catch (err) {
     return false;
   }
 };
 
-const api = new InstagramApi();
-
-test("Should fetch single photo (-caption)", async () => {
+test('Should fetch single photo (-caption)', async () => {
   expect.assertions(6);
 
   const postDetails = {
-    code: "CMmwQjXLVeF",
-    id: "2532924084156454789",
+    code: 'CMmwQjXLVeF',
+    id: '2532924084156454789',
   };
 
   try {
-    const post = await api.get(postDetails.code);
+    const post = await InstagramApi.get(postDetails.code);
     expect(post.id).toBe(postDetails.id);
     expect(post.code).toBe(postDetails.code);
     expect(post.is_video).toBe(false);
@@ -33,16 +31,16 @@ test("Should fetch single photo (-caption)", async () => {
   }
 });
 
-test("Should fetch single photo (+caption)", async () => {
+test('Should fetch single photo (+caption)', async () => {
   expect.assertions(6);
 
   const postDetails = {
-    code: "CMdYauRlD4W",
-    id: "2530285955161800214",
+    code: 'CMdYauRlD4W',
+    id: '2530285955161800214',
   };
 
   try {
-    const post = await api.get(postDetails.code);
+    const post = await InstagramApi.get(postDetails.code);
     expect(post.id).toBe(postDetails.id);
     expect(post.code).toBe(postDetails.code);
     expect(post.is_video).toBe(false);
@@ -54,16 +52,16 @@ test("Should fetch single photo (+caption)", async () => {
   }
 });
 
-test("Should fetch single video (+caption)", async () => {
+test('Should fetch single video (+caption)', async () => {
   expect.assertions(6);
 
   const postDetails = {
-    code: "CMK4fYqFiIe",
-    id: "2525078988478882334",
+    code: 'CMK4fYqFiIe',
+    id: '2525078988478882334',
   };
 
   try {
-    const post = await api.get(postDetails.code);
+    const post = await InstagramApi.get(postDetails.code);
     expect(post.id).toBe(postDetails.id);
     expect(post.code).toBe(postDetails.code);
     expect(post.is_video).toBe(true);
@@ -75,16 +73,16 @@ test("Should fetch single video (+caption)", async () => {
   }
 });
 
-test("Should fetch IGTV video (+caption)", async () => {
+test('Should fetch IGTV video (+caption)', async () => {
   expect.assertions(6);
 
   const postDetails = {
-    code: "CMVRFx8FIYl",
-    id: "2528001929847801381",
+    code: 'CMVRFx8FIYl',
+    id: '2528001929847801381',
   };
 
   try {
-    const post = await api.get(postDetails.code);
+    const post = await InstagramApi.get(postDetails.code);
     expect(post.id).toBe(postDetails.id);
     expect(post.code).toBe(postDetails.code);
     expect(post.is_video).toBe(true);
@@ -96,16 +94,16 @@ test("Should fetch IGTV video (+caption)", async () => {
   }
 });
 
-test("Should fetch photo album (+caption)", async () => {
+test('Should fetch photo album (+caption)', async () => {
   expect.assertions(6);
 
   const postDetails = {
-    code: "CMf29lRF52W",
-    id: "2530983242020396438",
+    code: 'CMf29lRF52W',
+    id: '2530983242020396438',
   };
 
   try {
-    const post = await api.get(postDetails.code);
+    const post = await InstagramApi.get(postDetails.code);
     expect(post.id).toBe(postDetails.id);
     expect(post.code).toBe(postDetails.code);
     expect(post.is_video).toBe(false);
@@ -114,35 +112,45 @@ test("Should fetch photo album (+caption)", async () => {
     expect(post.children).toEqual(
       expect.arrayContaining([
         expect.objectContaining({
-          id: "2530983238446620648",
-          code: "CMf29h8FB_o",
+          id: '2530983238446620648',
+          code: 'CMf29h8FB_o',
           is_video: false,
         }),
         expect.objectContaining({
-          id: "2530983238438257890",
-          code: "CMf29h7lITi",
+          id: '2530983238438257890',
+          code: 'CMf29h7lITi',
           is_video: false,
         }),
         expect.objectContaining({
-          id: "2530983238555903256",
-          code: "CMf29iCl6UY",
+          id: '2530983238555903256',
+          code: 'CMf29iCl6UY',
           is_video: false,
         }),
         expect.objectContaining({
-          id: "2530983238430039583",
-          code: "CMf29h7Fx4f",
+          id: '2530983238430039583',
+          code: 'CMf29h7Fx4f',
           is_video: false,
         }),
-      ])
+      ]),
     );
   } catch (err) {
     fail(err);
   }
 });
 
-test("Should fetch invalid post and return error", async () => {
+test('Should NOT fetch invalid post then throw an error', async () => {
   try {
-    const post = await api.get("AAAAAAAAA");
+    const post = await InstagramApi.get('!@#%-ok@q^$');
+    fail(post);
+  } catch (err) {
+    // Nothing
+    expect(err.message).toBe('Invalid post code.');
+  }
+});
+
+test('Should fetch invalid post then throw an error', async () => {
+  try {
+    const post = await InstagramApi.get('AbCdEf12_34');
     fail(post);
   } catch (err) {
     // Nothing
