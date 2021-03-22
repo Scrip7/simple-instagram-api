ad33674d2cee381bce489e54d3aab2709c1df274
Hooman <37535505+Scrip7@users.noreply.github.com>
2021-03-22 20:52:35 +0430

    improved tests

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,19 +1,31 @@
 import { InstagramApi } from "./../src/index";
+const URL = require("url").URL;
+
+const stringIsAValidUrl = (s: string) => {
+  try {
+    new URL(s);
+    return true;
+  } catch (err) {
+    return false;
+  }
+};
 
 const api = new InstagramApi();
 
-test("Post contains a single image without any caption", async () => {
+test("Should fetch single photo (-caption)", async () => {
   expect.assertions(6);
+
   const postDetails = {
-    code: "BR6iMU-A5xO",
-    id: "1475642208647617614",
+    code: "CMmwQjXLVeF",
+    id: "2532924084156454789",
   };
+
   try {
     const post = await api.get(postDetails.code);
     expect(post.id).toBe(postDetails.id);
     expect(post.code).toBe(postDetails.code);
     expect(post.is_video).toBe(false);
-    expect(post.url).toBeTruthy();
+    expect(stringIsAValidUrl(post.url)).toBe(true);
     expect(post.caption).toBeUndefined();
     expect(post.children).toStrictEqual([]);
   } catch (err) {
@@ -21,18 +33,20 @@ test("Post contains a single image without any caption", async () => {
   }
 });
 
-test("Post contains a single image with caption", async () => {
+test("Should fetch single photo (+caption)", async () => {
   expect.assertions(6);
+
   const postDetails = {
     code: "CMdYauRlD4W",
     id: "2530285955161800214",
   };
+
   try {
     const post = await api.get(postDetails.code);
     expect(post.id).toBe(postDetails.id);
     expect(post.code).toBe(postDetails.code);
     expect(post.is_video).toBe(false);
-    expect(post.url).toBeTruthy();
+    expect(stringIsAValidUrl(post.url)).toBe(true);
     expect(post.caption).toBeTruthy();
     expect(post.children).toStrictEqual([]);
   } catch (err) {
@@ -40,18 +54,20 @@ test("Post contains a single image with caption", async () => {
   }
 });
 
-test("Post contains a single IGTV video with a caption", async () => {
+test("Should fetch single video (+caption)", async () => {
   expect.assertions(6);
+
   const postDetails = {
-    code: "CMVRFx8FIYl",
-    id: "2528001929847801381",
+    code: "CMK4fYqFiIe",
+    id: "2525078988478882334",
   };
+
   try {
     const post = await api.get(postDetails.code);
     expect(post.id).toBe(postDetails.id);
     expect(post.code).toBe(postDetails.code);
     expect(post.is_video).toBe(true);
-    expect(post.url).toBeTruthy();
+    expect(stringIsAValidUrl(post.url)).toBe(true);
     expect(post.caption).toBeTruthy();
     expect(post.children).toStrictEqual([]);
   } catch (err) {
@@ -59,18 +75,41 @@ test("Post contains a single IGTV video with a caption", async () => {
   }
 });
 
-test("Gallery image with caption", async () => {
+test("Should fetch IGTV video (+caption)", async () => {
   expect.assertions(6);
+
+  const postDetails = {
+    code: "CMVRFx8FIYl",
+    id: "2528001929847801381",
+  };
+
+  try {
+    const post = await api.get(postDetails.code);
+    expect(post.id).toBe(postDetails.id);
+    expect(post.code).toBe(postDetails.code);
+    expect(post.is_video).toBe(true);
+    expect(stringIsAValidUrl(post.url)).toBe(true);
+    expect(post.caption).toBeTruthy();
+    expect(post.children).toStrictEqual([]);
+  } catch (err) {
+    fail(err);
+  }
+});
+
+test("Should fetch photo album (+caption)", async () => {
+  expect.assertions(6);
+
   const postDetails = {
     code: "CMf29lRF52W",
     id: "2530983242020396438",
   };
+
   try {
     const post = await api.get(postDetails.code);
     expect(post.id).toBe(postDetails.id);
     expect(post.code).toBe(postDetails.code);
     expect(post.is_video).toBe(false);
-    expect(post.url).toBeTruthy();
+    expect(stringIsAValidUrl(post.url)).toBe(true);
     expect(post.caption).toBeTruthy();
     expect(post.children).toEqual(
       expect.arrayContaining([
@@ -98,5 +137,14 @@ test("Gallery image with caption", async () => {
     );
   } catch (err) {
     fail(err);
+  }
+});
+
+test("Should fetch invalid post and return error", async () => {
+  try {
+    const post = await api.get("AAAAAAAAA");
+    fail(post);
+  } catch (err) {
+    // Nothing
   }
 });
